06a69953697c9101324e667dcba3d77539947f63
Jonathan Stark <abc518@gmail.com>
2022-09-18 15:14:53 -0400

    Remove a comment block, prettier couldn't process it.

--- src/renderer/components/CommitView.js
+++ src/renderer/components/CommitView.js
@@ -238,23 +238,6 @@ export function TagView({ selectedNode }) {
 }
 
 export function FileChanged(index, { file }) {
-  /*
-    files: Array(10)
-    0: additions: 2
-    blob_url:
-        "https://github.com/jstark518/Desktop-Deploy/blob/3824a4009637cfa0f0bb0c0adc7abcf7a5e81c60/.gitignore"
-    changes: 2
-    contents_url:
-        "https://api.github.com/repos/jstark518/Desktop-Deploy/contents/.gitignore?ref=3824a4009637cfa0f0bb0c0adc7abcf7a5e81c60"
-    deletions: 0
-    filename: ".gitignore"
-    patch:
-        "@@ -107,3 +107,5 @@ dist\n /.tmp/\n /.idea/\n /.env.json\n+/.cache.data.gh.json\n+.DS_Store"
-    raw_url:
-        "https://github.com/jstark518/Desktop-Deploy/raw/3824a4009637cfa0f0bb0c0adc7abcf7a5e81c60/.gitignore"
-    sha: "3655ab548616dba12cafa1ec9b3793b1f34b84fe"
-        status: "modified"
-     */
   return (
     <tr key={file.filename + index}>
       <td>{file.filename}</td>
