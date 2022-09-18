70ebee111f11c0c9d1246335dbb5cfbcf7009eb3
Restyled.io <commits@restyled.io>
2022-09-18 19:31:27 +0000

    Restyled by clang-format

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,17 +1,15 @@
 const rules = require('./webpack.rules');
 
 rules.push({
-    test: /\.css$/,
-    use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  test : /\.css$/,
+  use : [ {loader : 'style-loader'}, {loader : 'css-loader'} ],
 });
 
 module.exports = {
-    // Put your normal webpack config below here
-    devtool: "source-map",
-    module: {
-        rules,
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.ts', 'tsx']
-    }
+  // Put your normal webpack config below here
+  devtool : "source-map",
+  module : {
+    rules,
+  },
+  resolve : {extensions : [ '.js', '.jsx', '.ts', 'tsx' ]}
 };
