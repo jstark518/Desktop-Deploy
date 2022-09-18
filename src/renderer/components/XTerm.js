64702cef504c7bed9a3345fcdaa870664a664209
Restyled.io <commits@restyled.io>
2022-09-18 19:15:01 +0000

    Restyled by clang-format

--- src/renderer/components/XTerm.js
+++ src/renderer/components/XTerm.js
@@ -1,29 +1,29 @@
-import React, {useEffect, useRef, useState} from 'react';
-import { Terminal } from 'xterm';
 import {styled} from "@mui/system";
+import React, {useEffect, useRef, useState} from 'react';
+import {Terminal} from 'xterm';
+
 require("xterm/css/xterm.css");
 
 const XTermStyle = styled("div")({
-    maxHeight: "30vh",
-    flex: 0,
-    margin: "0 -1rem",
+  maxHeight : "30vh",
+  flex : 0,
+  margin : "0 -1rem",
 });
 
-
 export default function Xterm() {
-    const [term, setTerm] = useState(null);
-    const termElement = useRef(null);
-    useEffect(() => {
-        if(termElement) {
-            const terminal = new Terminal();
-            terminal.setOption('fontSize', 10);
-            terminal.open(termElement.current);
-            setTerm(terminal);
-            window.termAPI.onData((e, data) => terminal.write(data));
-            terminal.onData((e) => window.termAPI.send(e));
-            window.termAPI.ready();
-        }
-    }, []);
+  const [term, setTerm] = useState(null);
+  const termElement = useRef(null);
+  useEffect(() => {
+    if (termElement) {
+      const terminal = new Terminal();
+      terminal.setOption('fontSize', 10);
+      terminal.open(termElement.current);
+      setTerm(terminal);
+      window.termAPI.onData((e, data) => terminal.write(data));
+      terminal.onData((e) => window.termAPI.send(e));
+      window.termAPI.ready();
+    }
+  }, []);
 
-    return <XTermStyle ref={termElement}></XTermStyle>;
+  return <XTermStyle ref = {termElement}>< /XTermStyle>;
 }
