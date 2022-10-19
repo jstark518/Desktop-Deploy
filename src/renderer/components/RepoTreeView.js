ea146ccac2dfc757518fde4568e2898586958d16
Leo S <leo.sal.ca@outlook.com>
2022-10-19 13:14:53 -0600

    More improvements to the TreeView UI. Addind styled components

--- src/renderer/components/RepoTreeView.js
+++ src/renderer/components/RepoTreeView.js
@@ -2,21 +2,77 @@ import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import TreeItem, { treeItemClasses } from "@mui/lab/TreeItem";
 import TreeView from "@mui/lab/TreeView";
+import { styled } from "@mui/system";
 import React from "react";
 import { useEffect, useState } from "react";
-import { useAuth } from "./CustomHooks/useAuth";
 import RightArrowIcon from "./CustomIcons/RightArrowIcon";
 import { Box } from "@mui/system";
 import { Typography } from "@mui/material";
 
+const RepoStyledTreeItem = styled(TreeItem, {
+  shouldForwardProp: (props) => props !== "key" || props !== "nodeId" || props !== "label"})({
+    color: "black", 
+    borderRadius: "5px", 
+    backgroundColor: "#d9d9d9", 
+    fontFamily: "Roboto",
+    padding: '5px 0 5px 0', 
+    margin: "10px", 
+    [`& .${treeItemClasses.content}`]: {
+      '&.Mui-focused, &.Mui-selected, &.Mui-selected.Mui-focused': {
+        backgroundColor: "#CFEDD9",
+        border: "1px dotted black",               
+        borderRadius: "5px"
+      }
+    }
+  })
+
+function StyledTreeItem(props) {
+  const { labelText, labelIcon: LabelIcon, labelInfo, color, bgColor, ...other } = props;
+
+  return (
+    <RepoStyledTreeItem
+      label={
+        <Box sx={{ display: 'flex', alignItems: 'center', p: 0.5, pl: 1, pr: 1, m: 0 }}>
+          <LabelIcon color="inherit" sx={{ mr: 1 }} />
+          <Typography variant="body2" sx={{ fontWeight: 'inherit', flexGrow: 1 }}>
+            {labelText}
+          </Typography>
+          <Typography variant="caption" color="inherit">
+            {labelInfo}
+          </Typography>
+        </Box>
+      }
+      {...other}
+    />
+  );
+}
+
+const BranchStyledTreeItem = styled(TreeItem, {
+  shouldForwardProp: (props) => props !== "key" || props !== "nodeId" || props !== "label"})({
+    [`& .${treeItemClasses.label}`]: {
+      '&.MuiTreeItem-label': {
+        fontSize: "0.7rem",
+        overflowWrap: "break-word",
+      }
+    }
+  })
+
+const CommitStyledTreeItem = styled(TreeItem, {
+  shouldForwardProp: (props) => props !== "key" || props !== "nodeId" || props !== "label"})({
+    [`& .${treeItemClasses.label}`]: {
+      '&.MuiTreeItem-label': {
+        fontSize: "0.7rem",
+        overflowWrap: "break-word",
+        padding: "0 0 10px 0"
+      }
+    }
+  })
+
+
 export default function RepoTreeView({ onSelectNode, currentAuthType }) {
   const [repoData, setRepoData] = useState([]);
   const [selected, setSelected] = useState([]);
-  const {authType } = useAuth();
-
   const [listTitle, setListTitle] = useState('Repositories:');
-
-  
 
   const handleSelect = (event, nodeIds) => {
     setSelected(nodeIds);
@@ -50,13 +106,10 @@ export default function RepoTreeView({ onSelectNode, currentAuthType }) {
       onSelectNode({ selection: nodeData, repo: repoData[nodeData.repoIndex] });
     }
   };
-
+  /*
+    Determines which Repo list to display based on the currentAuthType
+  */
   useEffect(() => {
-    /*
-    Resolve the promise passed from index.js.
-    repo() and list() are exposed to window object from preload.js.
-    list() calls the API in index.js
-    */
    if (currentAuthType === "bitbucket") {
     window.repo.bblist().then((list) => {
       setListTitle('Bitbucket Repositories:');
@@ -77,26 +130,27 @@ export default function RepoTreeView({ onSelectNode, currentAuthType }) {
       defaultCollapseIcon={<ExpandMoreIcon />}
       defaultExpandIcon={<ChevronRightIcon />}
       /*
-        "TreeView" Prop, onNodeSelect, will fire a function.
-        It returns the value of prop "nodeId" from the selected node (TreeItem).
-        "nodeId" for each node has to be a string.
-        */
+        onNodeSelect will fire a function when a TreeItem is selected.
+        nodeId is returned as a string, so it must be converted to JSON.
+        We used nodeId to pass data from the TreeItem.
+      */
       onNodeSelect={handleSelect}
-      sx={{ padding: "20px" }}
+      sx={{ 
+        padding: "1rem",
+        [`& .${treeItemClasses.label}`]: {
+          '&.MuiTreeItem-label': {
+            fontSize: "0.8rem",
+          }
+        }
+      }}
     >
       <Typography variant="3" sx={{ fontSize: "20px", fontFamily: "Roboto", fontWeight: 400 }}>{listTitle}</Typography>
-      {/*
-        First use of the repoData, which was set in useEffect.
-        We map the Repos from GitHub into a "TreeView" component (MUI version of an ul).
-        Each Repo is in it's own "TreeItem" (MUI version of an li).
-        */}
       {repoData.map((repo, index) => (
         /*
-            We separate each branch, commit, and tag into a TreeItem, each nested in the current repo-TreeItem.
-            "nodeId" is a stringified JSON object, since "onNodeSelect" requires a string.
-            passing props from repo.branch, repo.commit, repo.tag to access elsewhere in the application.
-            */
-        <TreeItem
+          We separate each Repo into its Default Repo, branches, commits, and tags, placing each into a TreeItem.
+          "nodeId" is a stringified JSON object, since "onNodeSelect" requires a string.
+        */
+        <StyledTreeItem
           key={repo.name + index}
           nodeId={JSON.stringify({
             type: "repo",
@@ -104,39 +158,23 @@ export default function RepoTreeView({ onSelectNode, currentAuthType }) {
             name: repo.name,
             ...repo,
           })}
-          label={repo.name}
-          sx={{ 
-            color: "black", 
-            borderRadius: "5px", 
-            backgroundColor: "#d9d9d9", 
-            fontFamily: "Roboto",
-            fontSize: "1rem",
-            padding: '5px', 
-            margin: "10px", 
-            [`& .${treeItemClasses.content}`]: 
-              {'&.Mui-focused, &.Mui-selected, &.Mui-selected.Mui-focused': 
-                {
-                  backgroundColor: "#CFEDD9",
-                  borderRadius: "5px"
-                }
-              } 
-          }}
-        >
+          labelText={repo.name}
+          labelIcon={RightArrowIcon}
+        > 
           <TreeItem
             nodeId={JSON.stringify({ type: "branches", index })}
             label="Branches"
           >
             {repo.branches.map((branch, i) => (
-              <TreeItem
+              <BranchStyledTreeItem
                 key={branch.name + i}
                 nodeId={JSON.stringify({
                   type: "branch",
                   repoIndex: index,
                   ...branch,
                 })}
-                label={branch.name}
-
-              ></TreeItem>
+                label={branch.name}                
+              ></BranchStyledTreeItem>
             ))}
           </TreeItem>
           <TreeItem
@@ -144,15 +182,15 @@ export default function RepoTreeView({ onSelectNode, currentAuthType }) {
             label="Commits"
           >
             {repo.commits.map((commit, i) => (
-              <TreeItem
+              <CommitStyledTreeItem
                 key={"commit" + i}
                 nodeId={JSON.stringify({
                   type: "commit",
                   repoIndex: index,
                   ...commit,
                 })}
-                label={commit.message}
-              ></TreeItem>
+                label={commit.message.slice(0, 15)+"..."}
+              ></CommitStyledTreeItem>
             ))}
           </TreeItem>
           <TreeItem
@@ -171,7 +209,7 @@ export default function RepoTreeView({ onSelectNode, currentAuthType }) {
               ></TreeItem>
             ))}
           </TreeItem>
-        </TreeItem>
+        </StyledTreeItem>
       ))}
     </TreeView>
   );
